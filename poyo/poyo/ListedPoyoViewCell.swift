1f29ffa5f325b5afcebe56465a86710df77cb806
iTwicKz <twickes32@ufl.edu>
2016-03-08 14:19:11 -0500

    Made tableViewCells collapsible

--- poyo/poyo/ListedPoyoViewCell.swift
+++ poyo/poyo/ListedPoyoViewCell.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class ListedPoyoViewCell: UITableViewCell {
 
+    @IBOutlet weak var collapsedView: UIView!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var votesLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    class var expandedHeight: CGFloat { get { return 200 } }
+    class var defaultHeight: CGFloat { get { return 44 } }
+
+    
+    
+    
+    var frameAdded = false
+//    var expandedHeight: CGFloat = 200
+//    var defaultHeight: CGFloat = 44
+    
+    func checkHeight(){
+        collapsedView.hidden = (frame.size.height < ListedPoyoViewCell.expandedHeight)
+    }
+    
+    func watchFrameChanges(){
+        addObserver(self, forKeyPath: "frame", options: .New, context: nil)
+        if(!frameAdded){
+            checkHeight()
+            frameAdded = true
+        }
+    }
+    
+    func ignoreFrameChanges(){
+        if(frameAdded){
+            removeObserver(self, forKeyPath: "frame")
+            frameAdded = false
+        }
+    }
+
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if keyPath == "frame" {
+            checkHeight()
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
